adb887f1e931f60f7e25e459d1dcbde988780b87
bryanstark1 <bryandstark@icloud.com>
2023-07-16 14:35:07 -0400

    Added resume page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+// GET Home page
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
+});
+
+// GET About page
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+// GET Resumé page
+router.get('/resume', function(req, res, next) {
+  res.render('resume', { title: 'resume' });
 });
 
 module.exports = router;
